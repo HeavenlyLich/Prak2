b282d60054a5958465a3cb2b35d507a2b2dad3d9
HeavenlyLich <130435636+HeavenlyLich@users.noreply.github.com>
2023-09-29 19:15:39 +0300

    Add files via upload

---
+++ Pra2/Pra2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string s1 = "sasssdeerrfdrgjsoi";
+        int s2 = 123456789;
+        string s3 = "aasd poda chaaaar promt";
+        string s4 = "bla bla abra kadabra bro brat hello hello world";
+
+        Console.WriteLine($"1. Максимальна кількість послідовних символів\n '{s1}': \n {MaxConsecutiveChars(s1)}");
+        Console.WriteLine($"2. Вставити пробіли\n '{s2}':\n {InsertSpaces(s2.ToString())}");
+        Console.WriteLine($"3. Слово з найбільшою кількістю голосних\n '{s3}':\n {WordWithMostVowels(s3)}");
+        Console.WriteLine($"4. Нове речення без дублікатів\n '{s4}':\n {RemoveDuplicateWords(s4)}");
+    }
+
+    public static int MaxConsecutiveChars(string str)
+    {
+        if (string.IsNullOrEmpty(str)) return 0;
+
+        int max = 0;
+        int current = 1;
+        char previousChar = str[0];
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (str[i] == previousChar)
+            {
+                current++;
+            }
+            else
+            {
+                if (current > max)
+                {
+                    max = current;
+                }
+
+                previousChar = str[i];
+                current = 1;
+            }
+        }
+
+        return Math.Max(max, current);
+    }
+
+    public static string InsertSpaces(string str)
+    {
+        var reversedStr = new string(str.Reverse().ToArray());
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < reversedStr.Length; i++)
+        {
+            if (i % 3 == 0 && i != 0)
+            {
+                sb.Append(' ');
+            }
+
+            sb.Append(reversedStr[i]);
+        }
+
+        var result = new string(sb.ToString().Reverse().ToArray());
+        return result;
+    }
+
+    public static string WordWithMostVowels(string sentence)
+    {
+        var words = sentence.Split(' ');
+        var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+
+        string resultWord = null;
+        int maxVowels = 0;
+
+        foreach (var word in words)
+        {
+            int vowelCount = word.Count(c => vowels.Contains(c));
+
+            if (vowelCount > maxVowels)
+            {
+                maxVowels = vowelCount;
+                resultWord = word;
+            }
+        }
+
+        return resultWord;
+    }
+
+    public static string RemoveDuplicateWords(string sentence)
+    {
+        var words = sentence.Split(' ');
+        var wordCounts = new Dictionary<string, int>();
+
+        foreach (var word in words)
+        {
+            if (wordCounts.ContainsKey(word))
+            {
+                wordCounts[word]++;
+            }
+            else
+            {
+                wordCounts[word] = 1;
+            }
+        }
+
+        var resultSentence = string.Join(" ", wordCounts.Where(kvp => kvp.Value == 1).Select(kvp => kvp.Key));
+
+        return resultSentence;
+    }
+
+}
---
+++ examples/examples/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Приклад 1:");
+
+string s = "Dfg+t5-r7y;asd*s34;rt";
+int t = s.IndexOf(';');
+int i, k = 0;
+for (i = 0; i <= t; i++)
+{
+    if (char.IsLetter(s[i])) k++;
+}
+Console.WriteLine(k);
+Console.ReadKey();
+
+Console.WriteLine("Приклад 2:");
+string s1 = "АУАУАПАУК";
+int i1 = 0;
+do
+{
+    string ss = s1.Substring(i1,2);
+    if (ss == "АУ")
+    {
+        s1 = s1.Insert(i1, "O"); i1 = i1 + 3;
+    }
+    else
+    {
+        i1++;
+    };
+}
+while (i1 < s1.Length -1);
+Console.WriteLine(s1);
+Console.ReadKey();
+
+Console.WriteLine("Приклад 3");
+string s2 = "біологія алгебра історія географія геометрія";
+string c2 = "і";
+string[] a;
+a = s2.Split(' ');
+int i2;
+for (i2 = 0; i2 < a.Length; i2++)
+{
+    int t2 = a[i2].IndexOf(c2);
+    if (t2 != -1) Console.WriteLine(a[i2]);
+}
+Console.ReadKey();
+
+Console.WriteLine("Приклад 4");
+string s3 = "біологія алгебра історія географія геометрія";
+string[] a3;
+a3 = s3.Split(" ");
+Array.Reverse(a3);
+string s4 = string.Join(" ", a3);
+Console.WriteLine(s4);
+Console.ReadKey();
